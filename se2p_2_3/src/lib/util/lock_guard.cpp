5ca7ba3a108fced0b61c858b61ab47e82a9ce977
Marian Triebe <m.triebe@live.de>
2014-10-17 01:17:21 +0200

    Add several wrapper classes This includes wrappers for `pthread_cond_t`, `pthread_mutex_t`. This also add a implementation of the `scoped locking` Pattern. Namespace for these kind of files is `se2::util`.

---
+++ se2p_2_3/src/lib/util/lock_guard.cpp
@@ -0,0 +1,35 @@
+/******************************************************************************
+ *                   ____    ____       ___    ____                           *
+ *                  /\  _`\ /\  _`\   /'___`\ /\  _`\                         *
+ *                  \ \,\L\_\ \ \L\_\/\_\ /\ \\ \ \L\ \                       *
+ *                   \/_\__ \\ \  _\L\/_/// /__\ \ ,__/                       *
+ *                     /\ \L\ \ \ \L\ \ // /_\ \\ \ \/                        *
+ *                     \ `\____\ \____//\______/ \ \_\                        *
+ *                      \/_____/\/___/ \/_____/   \/_/                        *
+ *                                                                            *
+ * Copyright (C) 2014                                                         *
+ * Katja Kirstein    <katja.kirstein    (at) haw-hamburg.de>                  *
+ * Anne-Lena Kowalka <anne-lena.kowalka (at) haw-hamburg.de>                  *
+ * Marian Triebe     <marian.triebe     (at) haw-hamburg.de>                  *
+ * Eugen Winter      <eugen.winter      (at) haw-hamburg.de>                  *
+ *                                                                            *
+ * Gruppe 2.3                                                                 *
+ ******************************************************************************/
+/**
+ * @file    lock_guard.cpp
+ * @version 0.1
+ *
+ * Implementierung des `Scoped Locking` Pattern
+ **/
+
+#include "lock_guard.hpp"
+
+using namespace se2::util;
+
+lock_guard::lock_guard(mutex& _mutex) : m_ref(_mutex) {
+  m_ref.acquire();
+}
+
+lock_guard::~lock_guard() {
+  m_ref.release();
+}
---
+++ se2p_2_3/src/lib/util/mutex.cpp
@@ -0,0 +1,43 @@
+/******************************************************************************
+ *                   ____    ____       ___    ____                           *
+ *                  /\  _`\ /\  _`\   /'___`\ /\  _`\                         *
+ *                  \ \,\L\_\ \ \L\_\/\_\ /\ \\ \ \L\ \                       *
+ *                   \/_\__ \\ \  _\L\/_/// /__\ \ ,__/                       *
+ *                     /\ \L\ \ \ \L\ \ // /_\ \\ \ \/                        *
+ *                     \ `\____\ \____//\______/ \ \_\                        *
+ *                      \/_____/\/___/ \/_____/   \/_/                        *
+ *                                                                            *
+ * Copyright (C) 2014                                                         *
+ * Katja Kirstein    <katja.kirstein    (at) haw-hamburg.de>                  *
+ * Anne-Lena Kowalka <anne-lena.kowalka (at) haw-hamburg.de>                  *
+ * Marian Triebe     <marian.triebe     (at) haw-hamburg.de>                  *
+ * Eugen Winter      <eugen.winter      (at) haw-hamburg.de>                  *
+ *                                                                            *
+ * Gruppe 2.3                                                                 *
+ ******************************************************************************/
+/**
+ * @file    mutex.cpp
+ * @version 0.1
+ *
+ * `pthread_mutex_t` Wrapper
+ **/
+
+#include "mutex.hpp"
+
+using namespace se2::util;
+
+mutex::mutex() {
+  pthread_mutex_init(&m_mutex, NULL);
+}
+
+mutex::~mutex() {
+  pthread_mutex_destroy(&m_mutex);
+}
+
+int mutex::acquire() {
+  return pthread_mutex_lock(&m_mutex);
+}
+
+int mutex::release() {
+  return pthread_mutex_unlock(&m_mutex);
+}
