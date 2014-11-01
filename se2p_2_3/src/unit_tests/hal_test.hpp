36d3a18f7e60dbfba73afeedf423091d3e5fceac
Katezilla <kirsteinkatja@gmail.com>
2014-11-01 22:04:34 +0100

    Add HAL unit tests

---
+++ se2p_2_3/src/unit_tests/hal_test.hpp
@@ -0,0 +1,43 @@
+/******************************************************************************
+ *                   ____    ____       ___    ____                           *
+ *                  /\  _`\ /\  _`\   /'___`\ /\  _`\                         *
+ *                  \ \,\L\_\ \ \L\_\/\_\ /\ \\ \ \L\ \                       *
+ *                   \/_\__ \\ \  _\L\/_/// /__\ \ ,__/                       *
+ *                     /\ \L\ \ \ \L\ \ // /_\ \\ \ \/                        *
+ *                     \ `\____\ \____//\______/ \ \_\                        *
+ *                      \/_____/\/___/ \/_____/   \/_/                        *
+ *                                                                            *
+ * Copyright (C) 2014                                                         *
+ * Katja Kirstein    <katja.kirstein    (at) haw-hamburg.de>                  *
+ * Anne-Lena Kowalka <anne-lena.kowalka (at) haw-hamburg.de>                  *
+ * Marian Triebe     <marian.triebe     (at) haw-hamburg.de>                  *
+ * Eugen Winter      <eugen.winter      (at) haw-hamburg.de>                  *
+ *                                                                            *
+ * Gruppe 2.3                                                                 *
+ ******************************************************************************/
+/**
+ * @file    hal_test.hpp
+ * @version 0.1
+ *
+ * Unit tests der HAL
+ **/
+
+#include "lib/hal/HWaccess.hpp"
+
+class hal_test {
+ public:
+  /**
+   * Constructor
+   **/
+  hal_test ();
+
+  /**
+   * Destructor
+   **/
+  ~hal_test();
+
+ /**
+  * Start hal test
+  **/
+  int start_test();
+};
