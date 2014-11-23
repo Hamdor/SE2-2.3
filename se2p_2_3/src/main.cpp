1f3c1d6f0d7ff48ef58f737ea42d90602aca90fd
Eugen Winter <eugen.winter@haw-hamburg.de>
2014-11-23 15:45:33 +0100

    Added simple simulation test in main.

--- se2p_2_3/src/main.cpp
+++ se2p_2_3/src/main.cpp
@@ -7,6 +7,10 @@
 
 #include "lib/hal/HWaccess.hpp"
 #include "lib/util/singleton_mgr.hpp"
+#include "lib/dispatcher/dispatcher.hpp"
+#include "lib/fsm/events.hpp"
+#include "lib/fsm/token.hpp"
+#include "lib/fsm/state.hpp"
 
 #include "unit_tests/irq_test.hpp"
 #include "unit_tests/test_suite.hpp"
@@ -21,6 +25,8 @@
 
 using namespace std;
 using namespace se2;
+using namespace se2::fsm;
+using namespace se2::dispatch;
 using namespace se2::hal;
 using namespace se2::util;
 using namespace se2::unit_tests;
@@ -29,21 +35,36 @@ int main(int argc, char *argv[]) {
 #ifdef SIMULATION
   IOaccess_open();
 #endif
+//  /**
+//   * Unit Tests
+//   **/
+//  cout << "run `hal_test_stub1`  errors: "
+//       << test_suite<hal_test_stub1>().run()  << endl;
+//  cout << "run `hal_test_stub2`  errors: "
+//       << test_suite<hal_test_stub2>().run()  << endl;
+//  cout << "run `irq_test`        errors: "
+//       << test_suite<irq_test>().run()        << endl;
+//  cout << "run `dispatcher_test` errors: "
+//       << test_suite<dispatcher_test>().run() << endl;
+//#ifdef HAS_SERIAL_INTERFACE
+//  cout << "run `serial_test`     errors: "
+//       << test_suite<serial_test>().run()     << endl;
+//#endif
   /**
-   * Unit Tests
+   * Token Test
    **/
-  cout << "run `hal_test_stub1`  errors: "
-       << test_suite<hal_test_stub1>().run()  << endl;
-  cout << "run `hal_test_stub2`  errors: "
-       << test_suite<hal_test_stub2>().run()  << endl;
-  cout << "run `irq_test`        errors: "
-       << test_suite<irq_test>().run()        << endl;
-  cout << "run `dispatcher_test` errors: "
-       << test_suite<dispatcher_test>().run() << endl;
-#ifdef HAS_SERIAL_INTERFACE
-  cout << "run `serial_test`     errors: "
-       << test_suite<serial_test>().run()     << endl;
-#endif
+
+  // Ampel ansteuern
+  hwaccess* hal = TO_HAL(singleton_mgr::get_instance(HAL_PLUGIN));
+  hal->set_light(GREEN, 1);
+  std::cout << "main: Ampel wurde auf Gruen geschaltet" << std::endl;
+
+  token* t = new token();
+  t->set_state(new anonymous_token(t));
+  std::cout << "main: Token wurde erstellt und auf Status anonymous_token gesetzt" << std::endl;
+
+  while(1);
+
   singleton_mgr::shutdown();
 #ifdef SIMULATION
   IOaccess_close();
